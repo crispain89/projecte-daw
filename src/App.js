67ea084c6d7a2bc61032ea02a7c95d31f5f6c978
Cristina Martinez <cristinaaran89@gmail.com>
2022-04-27 21:15:21 +0200

    [inicio y registro semiacabado]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import './commons/css/estilos.css'
 import logo from './logo.svg';
 import {Login} from './commons/js/Login'
 import {Register} from './commons/js/Register'
-import {Button} from 'react-bootstrap'
+import {Button, Row, Col} from 'react-bootstrap'
 import React, { useState } from 'react';
 
 
@@ -9,17 +10,26 @@ import './App.css';
 
 function App() {
 
-const  [mostrar , setMostrar]=useState (false);
+const  [mostrar , setMostrar] = useState (false);
 
 
   return (
     <>
-      
-      {mostrar ? <Button variant='outline-primary' onClick={()=>setMostrar(false)} name='Login'>Login</Button> 
+      <h1 className="bienvenido">Bienvenidos a Cram</h1>
+      { mostrar ?
+      <div className="container">
+       <Register setMostrar={setMostrar} ver={mostrar}/> 
+       </div>
+       :
+      <div className="container__login">
+
+        <Login setMostrar={setMostrar} ver={mostrar}/>
+      </div>
+      }
+     {/*  {mostrar ? <Button variant='outline-primary' onClick={()=>setMostrar(false)} name='Login'>Login</Button> 
       : 
-      <Button variant='outline-primary' onClick={()=>setMostrar(true)} name='Registrate'>Registrate</Button>}
-      
-      { mostrar ? <Register/> : <Login/>}
+      <Button className='boton__auxiliar' variant='outline-primary' onClick={()=>setMostrar(true)} name='Registrate'>Registrate</Button>} */}
+    <Footer> </Footer>
       
     </>
    
