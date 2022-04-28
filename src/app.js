6182231228ed88d5e07dfa3b5c2571bb5ed56db9
Marc <macape@fp.insjoaquimmir.cat>
2022-04-28 15:47:48 +0200

    test usuarios supertest, usuario extra validations

--- src/app.js
+++ src/app.js
@@ -27,8 +27,6 @@ app.use('/api', express.json())
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+
 
 module.exports = app;
--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,32 +1,40 @@
 module.exports = (sequelize, Sequelize) => {
   const Usuario = sequelize.define("usuario", {
     nombre: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     apellidos: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: true,
     },
     contraseña: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     email: {
       type: Sequelize.STRING,
       validate:{
         isEmail: true
-      }
+      },
+      allowNull: false,
     },
     fecha_nacimiento: {
-      type: Sequelize.DATEONLY
+      type: Sequelize.DATEONLY,
+      allowNull: true,
     },
     dni: {
       type: Sequelize.STRING,
-      unique: true
+      unique: true,
+      allowNull: false,
     },
     rol_id: {
-      type: Sequelize.INTEGER
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     avatar_id: {
-      type: Sequelize.INTEGER
+      type: Sequelize.INTEGER,
+      allowNull: false,
     }
   },{
     timestamps: false
