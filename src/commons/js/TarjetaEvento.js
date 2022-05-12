00a6a725bbb96f872fe11f54b7d9548c76376115
crispain89 <cristinaaran89@gmail.com>
2022-05-12 12:04:19 +0200

    add de descripcion y modificacion de css para la card de eventos

--- src/commons/js/TarjetaEvento.js
+++ src/commons/js/TarjetaEvento.js
@@ -1,23 +1,27 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card ,Badges} from "react-bootstrap";
 
-export default function Evento({nombre,edicion,lugar,src}){
+export default function Evento({nombre,edicion,descripcion,inicio,final,lugar,src}){
 
     return(
         <Card>
-            <Card.Header>Evento</Card.Header>
+            
             <Card.Body>
-                <Card.Title>{nombre}{edicion}</Card.Title>
-                {console.log(src)}
+                <div className='card__evento'>
+                    <Card.Img className='imagen__card' variant="top" src={src} />
+                    <div className='card__body'>
+                        <Card.Title>{nombre}{edicion}</Card.Title>
+                        {console.log(src)}
+                        <Card.Text>
+                        <li> {descripcion}</li>
+                        <br />
+                        <li>El evento se realizará en : <span className='card__styleInfo'>{lugar}</span></li>
+                        <li>El evento se realizará el: <span className='card__styleInfo'>{inicio}</span></li>
+                        <li> El evento finalizará el: <span className='card__styleInfo'>{final}</span></li>
+                        </Card.Text>
 
-                <Card.Img className='imagen__card' variant="top" src={src} />
-                {console.log(src)}
-                <Card.Text>
-                    
-                    <li>{lugar}</li>
-                    
-
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                    </div>
+                </div>
+                <Button variant="primary">ver evento</Button>
             </Card.Body>
         </Card>
     )
