e710b0bef3895b697afb689dc0549dc223731db2
macape01 <macape@fp.insjoaquimmir.cat>
2022-05-16 20:09:07 +0200

    update user profile service, form retocado

--- src/components/user/UserProfile.js
+++ src/components/user/UserProfile.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef, useContext } from 'react'
 import { Col, Form, Image, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router'
 import UsuariosService from '../../servicios/usuarios.service'
 import { AuthContext } from '../context/AuthContext'
 
 export default function UserProfile(){
-    const {user,setUser} = useContext(AuthContext)
+    const {user,setUser,logout} = useContext(AuthContext)
     const [selectedFile,setSelectedFile] = useState()
+    const [form,setForm] = useState({})
+    const navigate = useNavigate()
 
     const handleFileChange = (e) => {
         setSelectedFile(e.target.files[0])
@@ -45,9 +48,34 @@ export default function UserProfile(){
         }
     }
 
+    const handleRedirect = (e) => {
+        e.preventDefault();
+        logout();
+        navigate("/forgot");
+    }
+
+    const handleUpdateProfile = async (e) => {
+        //Hacer fecha nacimiento tipo date
+        //Comprobar que los valores no esten vacios
+        e.preventDefault();
+        console.log("FORM",form)
+        try{
+            let res = await UsuariosService.update("usuarios",user.id,form)
+            if (res.status === 200){
+                setUser({...user,...form})
+            }
+        }
+        catch(e){
+            if ( e.response.status === 500 ){
+                alert("Ha habido un problema al actualizar el usuario")
+            }
+        }
+    }
+
     return(
         <div className='d-flex justify-content-center align-items-center'>
-            <Form className={"bg-light rounded p-4 w-75"}>
+            <div className={"bg-light rounded p-4 w-100"}>
+            <Form >
             <h1>Perfil de Usuario</h1>
                 <fieldset className='customLegend'>
                     <legend>Avatar</legend>
@@ -66,65 +94,67 @@ export default function UserProfile(){
                     <legend>Datos</legend>
                     <Col>
                         <Form.Group as={Col} className="mb-3" controlId="formPlaintextPassword">
-                            <Form.Label column sm="2">
+                            <Form.Label column>
                             Nombre
                             </Form.Label>
                             <Col sm="10">
-                            <Form.Control readOnly defaultValue="contra" />
+                            <Form.Control readOnly defaultValue={user.nombre || "Sin definir"} />
                             </Col>
                         </Form.Group>
                         <Form.Group as={Col} className="mb-3" controlId="formPlaintextPassword">
-                            <Form.Label column sm="2">
+                            <Form.Label column>
                             Apellidos
                             </Form.Label>
                             <Col sm="10">
-                            <Form.Control readOnly defaultValue="contra" />
+                            <Form.Control readOnly={!!user.apellidos} onChange={(e)=>setForm({...form,apellidos:e.target.value})} value={user.apellidos || "Sin definir"} />
                             </Col>
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group as={Col} className="mb-3" controlId="formPlaintextEmail">
-                            <Form.Label column sm="2">
+                            <Form.Label column>
                             Email
                             </Form.Label>
                             <Col sm="10">
-                            <Form.Control readOnly defaultValue="email@example.com" />
+                            <Form.Control readOnly defaultValue={user.email || "Sin definir"} />
                             </Col>
                         </Form.Group>
 
                         <Form.Group as={Col} className="mb-3" controlId="formPlaintextPassword">
-                            <Form.Label column sm="2">
+                            <Form.Label column>
                             Telefono
                             </Form.Label>
                             <Col sm="10">
-                            <Form.Control readOnly defaultValue="contra" />
+                            <Form.Control readOnly={!!user.telefono} onChange={(e)=>setForm({...form,telefono:e.target.value})} value={user.telefono || "Sin definir"} />
                             </Col>
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group as={Col} className="mb-3" controlId="formPlaintextEmail">
-                            <Form.Label column sm="2">
-                            Poblacion
-                            </Form.Label>
-                            <Col sm="10">
-                            <Form.Control readOnly defaultValue="email@example.com" />
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Col} className="mb-3" controlId="formPlaintextEmail">
-                            <Form.Label column sm="2">
+                            <Form.Label column>
                             DNI
                             </Form.Label>
                             <Col sm="10">
-                            <Form.Control readOnly defaultValue="email@example.com" />
+                            <Form.Control readOnly defaultValue={user.dni || "Sin definir"}/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Col} className="mb-3" controlId="formPlaintextEmail">
+                            <Form.Label column>
+                            Fecha nacimiento
+                            </Form.Label>
+                            <Col sm="10">
+                            <Form.Control type="date" readOnly={!!user.fecha_nacimiento} onChange={(e)=>setForm({...form,fecha_nacimiento:e.target.value})} value={user.fecha_nacimiento || "Sin definir"} />
                             </Col>
                         </Form.Group>
                     </Col>
+                    <button onClick={handleUpdateProfile} className='btn mb-3 btn-primary'>Actualizar perfil</button>
                 </fieldset>
+            </Form>
                 <h5 className="bg-info profile__forgot rounded">
                     <span className='text-secondary'>Olvidaste la contraseña?</span>
-                    <Link to="/forgot">Recuperar</Link>
+                    <Link onClick={handleRedirect} to="/forgot">Recuperar</Link>
                 </h5>
-            </Form>
+            </div>
         </div>   
     )
 } 
