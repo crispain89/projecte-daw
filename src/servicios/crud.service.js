8591f028e3a4da949726d429bee3642b718b16f8
macape01 <macape@fp.insjoaquimmir.cat>
2022-05-16 20:09:07 +0200

    update user profile service, form retocado

--- src/servicios/crud.service.js
+++ src/servicios/crud.service.js
@@ -6,10 +6,14 @@ export default class ApiCrudService{
     static index(resource) {
         return httpC.get(`/${resource}`)
     }
-   static show(resource,id){
+    static show(resource,id){
        return httpC.get(`/${resource}/${id}`)
     }
     static create(resource,data){
         return httpC.post(`/${resource}`,data)
+    }
+    static update(resource,id,data){
+        console.log(`/${resource}/${id}`)
+        return httpC.put(`/${resource}/${id}`,data)
     }
 }
