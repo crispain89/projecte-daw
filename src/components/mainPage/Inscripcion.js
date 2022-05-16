f67611ab0135151098aba09ca679611dce58b991
crispain89 <cristinaaran89@gmail.com>
2022-05-16 23:01:04 +0200

    inscripcion a eventos con la tabla de inscripciones

--- src/components/mainPage/Inscripcion.js
+++ src/components/mainPage/Inscripcion.js
@@ -23,6 +23,8 @@ useEffect(() => {
         const eventos = await EventosService.getEventosCurrent("eventos")
         console.log("eventos para el select:=>",eventos.data)
         setEventos(eventos.data)
+        console.log("22222222222222222222222222",eventos)
+       
       }catch(e){
         console.log(e)
       }finally{
@@ -42,18 +44,21 @@ const handleSubmit = async(e) => {
     
 		}))
 		setDatos(newIns)
-		console.log("AAAAARRRAYYY",newIns)
+		
 
 		await Promise.all(newIns.map(async(part)=>{
 			console.log("PARTTTT",part)
 			return await ApiCrudService.create("inscripciones",{id_usuario:part.data.id,id_evento:idEvento})
 
 		}))
+    let coun=eventos.length
+    console.log("EVENTOS",eventos)
+    alert("Se han inscrito los usuarios correctamente en la carrera .")
     
     }catch(e){
       console.log(e)
     }
-  }
+}
   
 const handleSelect=(e)=>{
 	let idEvento=e.target.value
@@ -78,6 +83,7 @@ const handleFile=(e)=>{
 
 return (
   <>
+    
     <Form.Label>Escoge un evento para inscribir a los participantes</Form.Label>
     <Form.Select aria-label="Escoge un evento" onChange={(e)=>handleSelect(e)}>
 		{
@@ -96,6 +102,8 @@ return (
         Enviar
       </Button>
     </Form>
+
+    
   </>
 )
 }
