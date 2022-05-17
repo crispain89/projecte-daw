bb25b638a676fe13944051930b500591efbe1d4d
macape01 <macape@fp.insjoaquimmir.cat>
2022-05-17 13:49:24 +0200

    register, login, home layout redesigned

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -67,54 +67,56 @@ export function Login() {
     }
 
   return (
-      <div className="container__login">
-        <Form className="justify-content-center "  onSubmit={handleSubmit}>
-            <h3 className="componente__titulo" >Login</h3>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Dirección de email</Form.Label>
-                    <Form.Control 
-                    name='email'
-                    size='sm' 
-                    type="email" 
-                    placeholder="introduce tu email" 
-                    onChange={(e)=>setForm({...form, email:e.target.value})}/>
+      <div className="container w-50 mt-5 bg-light rounded p-3">
+        <div className="row justify-content-center">
+            <Form onSubmit={handleSubmit}>
+                <h3 className="componente__titulo" >Login</h3>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Dirección de email</Form.Label>
+                        <Form.Control 
+                        name='email'
+                        size='sm' 
+                        type="email" 
+                        placeholder="introduce tu email" 
+                        onChange={(e)=>setForm({...form, email:e.target.value})}/>
+                        <Form.Text className="text-muted">
+                        Estos datos no se van a compartir
+                        </Form.Text>  
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Contaseña</Form.Label>
+                    <Form.Control type="password" placeholder="password" onChange={(e)=>setForm({...form, password:e.target.value})}/>
                     <Form.Text className="text-muted">
-                    Estos datos no se van a compartir
-                    </Form.Text>  
-            </Form.Group>
+                    Mínimo 8 caracteres, una letra Mayúscula y un número
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" onChange={()=>setRemember(!remember)} label="Recuerdame" />
+                </Form.Group>
+                <Button className="botones__login" variant="primary" type="submit">
+                    Login
+                </Button>
+                <Button className="botones__login" onClick={()=>HandleRedirect()} variant="secondary">
+                    Registrate
+                </Button>
+                <Link to="/forgot" className='style__forgot'><span>¿Has olvidado la contraseña?</span></Link>
+            </Form>  
+            <Modal show={show} onHide={toggleShow}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Resend email</Modal.Title>
+                </Modal.Header>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contaseña</Form.Label>
-                <Form.Control type="password" placeholder="password" onChange={(e)=>setForm({...form, password:e.target.value})}/>
-                <Form.Text className="text-muted">
-                Mínimo 8 caracteres, una letra Mayúscula y un número
-                </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" onChange={()=>setRemember(!remember)} label="Recuerdame" />
-            </Form.Group>
-            <Button className="botones__login" variant="primary" type="submit">
-                Login
-            </Button>
-            <Button className="botones__login" onClick={()=>HandleRedirect()} variant="secondary">
-                Registrate
-            </Button>
-            <Link to="/forgot" className='style__forgot'><span>¿Has olvidado la contraseña?</span></Link>
-        </Form>  
-        <Modal show={show} onHide={toggleShow}>
-            <Modal.Header closeButton>
-                <Modal.Title>Resend email</Modal.Title>
-            </Modal.Header>
+                <Modal.Body>
+                    <p>Tu cuenta no ha sido verificado todavia, dale click a enviar para recibir un email de confirmacion</p>
+                </Modal.Body>
 
-            <Modal.Body>
-                <p>Tu cuenta no ha sido verificado todavia, dale click a enviar para recibir un email de confirmacion</p>
-            </Modal.Body>
-
-            <Modal.Footer>
-                <Button onClick={toggleShow} variant="secondary">Cerrar</Button>
-                <Button onClick={handleResend} variant="primary">Enviar</Button>
-            </Modal.Footer>
-        </Modal>
+                <Modal.Footer>
+                    <Button onClick={toggleShow} variant="secondary">Cerrar</Button>
+                    <Button onClick={handleResend} variant="primary">Enviar</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
         </div>    
   )
 }
